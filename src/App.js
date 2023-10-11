94de1102b7e5887f29a56f5f9b8fe4d843bd11ce
Ester2109 <126612066+Ester2109@users.noreply.github.com>
2023-10-11 16:35:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to asdouhasdoiud.
+          Edit <code>src/App.js</code> and save to kakestykke.
         </p>
         <a
           className="App-link"
